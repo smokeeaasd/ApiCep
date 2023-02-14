7b79c52b4f0198d98555a202a324671ca9afa8f9
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-02-14 16:12:08 -0300

    Primeiro Commit

---
+++ Controller/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ApiCep\Controller;
+
+abstract class Controller
+{
+    protected static function getResponseAsJSON($data)
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=utf-8");
+        header("Cache-Control: no-cache, must-revalidate");
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Pragma: public");
+
+        exit(json_encode($data));
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include 'config.php';
+include 'autoload.php';
+include 'rotas.php';
+
+?>
