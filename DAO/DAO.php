0558388c72c96b3e4b192bd524f67da485b7eff9
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-03-02 16:02:47 -0300

    adicionando funçôes no dao.php

---
+++ DAO/DAO.php
@@ -0,0 +1,35 @@
+<?php
+
+use Exception;
+use \PDO;
+use PDOException;
+
+abstract class DAO extends PDO
+{
+    protected $conexao;
+
+    public function __construct()
+    {
+        try
+        {
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+            ];
+
+            $dsn = "mysql:host=" . $_ENV['db']['host'] . ";dbname=" . $_ENV['db']['database'];
+
+            $this->conexao = new PDO(
+                $dsn, $_ENV['db']['user'],
+                $_ENV['db']['pass'],
+                $options
+            );
+        }
+        catch (PDOException $e)
+        {
+            throw new Exception("Ocorreu um erro ao tentar conectar do banco de dados.");
+        }
+    }
+}
+
+?>
