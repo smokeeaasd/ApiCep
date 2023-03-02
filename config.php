a1ccbddc1bd3b3146456c117bf9a18ca7c88bf8a
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-03-02 16:11:34 -0300

    adicionando banco de dados na config

--- config.php
+++ config.php
@@ -5,5 +5,6 @@ define('BASEDIR', dirname(__FILE__, 2));
 $_ENV['db']['host'] = 'localhost:3307';
 $_ENV['db']['user'] = 'root';
 $_ENV['db']['pass'] = 'etecjau';
+$_ENV['db']['database'] = 'busca_by_cep';
 
 ?>
