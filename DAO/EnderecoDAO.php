48ee84763fd57d5eaa51c27cf2a924ad4df082bc
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-03-07 15:56:03 -0300

    adicionando métodos DAO

--- DAO/EnderecoDAO.php
+++ DAO/EnderecoDAO.php
@@ -2,7 +2,25 @@
 
 class EnderecoDAO extends DAO
 {
-    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function selectByCep(int $cep)
+    {
+        $sql = "SELECT * FROM logradouro WHERE cep = ?";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $cep);
+        $stmt->execute();
+
+        $endereco_obj = $stmt->fetchObject("App\Model\EnderecoModel");
+
+        $endereco_obj->arr_cidades = $this->selectCidadesByUf($endereco_obj->UF);
+
+        return $endereco_obj;
+    }
 }
 
 ?>
