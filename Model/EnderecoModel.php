70f8a88dcaabcd946cdbf5361c4abecf6a559e98
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-03-09 15:59:13 -0300

    Model de endereços

--- Model/EnderecoModel.php
+++ Model/EnderecoModel.php
@@ -4,5 +4,6 @@ namespace ApiCep\Model;
 
 class EnderecoModel extends Model
 {
-    
+    public $id_logradouro, $tipo, $descricao, $id_cidade, $uf, $complemento,
+    $descricao_sem_numero, $descricao_cidade, $codigo_cidade_ibge, $descricao_bairro;
 }
