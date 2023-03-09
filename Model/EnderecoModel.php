562de7d3f6f3462c3038c280cb34a023ad024df1
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-03-09 16:06:39 -0300

    adição de métodos Model

--- Model/EnderecoModel.php
+++ Model/EnderecoModel.php
@@ -2,8 +2,26 @@
 
 namespace ApiCep\Model;
 
+use EnderecoDAO;
+use Exception;
+
 class EnderecoModel extends Model
 {
     public $id_logradouro, $tipo, $descricao, $id_cidade, $uf, $complemento,
-    $descricao_sem_numero, $descricao_cidade, $codigo_cidade_ibge, $descricao_bairro;
+        $descricao_sem_numero, $descricao_cidade, $codigo_cidade_ibge, $descricao_bairro;
+
+    public $arr_cidades;
+
+    public function getLogradouroByBairroAndCidade(string $bairro, int $id_cidade)
+    {
+        try
+        {
+            $dao = new EnderecoDAO();
+
+            $this->rows = $dao->selectLogradouroByBairroAndCidade($bairro, $id_cidade);
+        } catch (Exception $ex)
+        {
+            exit("Erro encontrado: " . $ex->getMessage());
+        }
+    }
 }
