568423bffd0cd8f329e5647fd993babc63cb58b1
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-03-14 15:53:32 -0300

    Adicionando métodos na EnderecoController

--- Controller/EnderecoController.php
+++ Controller/EnderecoController.php
@@ -2,14 +2,25 @@
 
 namespace ApiCep\Controller;
 
-use App\Model\{ EnderecoModel, CidadeModel };
+use ApiCep\Model\EnderecoModel;
+use App\Model;
 use Exception;
 
 final class EnderecoController extends Controller
 {
     public static function getCepByLogradouro() : void
     {
+        try
+        {
+            $logradouro = $_GET['logradouro'];
 
+            $model = new EnderecoModel();
+            $model->getCepByLogradouro($logradouro);
+
+            parent::getResponseAsJSON($model->rows);
+        } catch (Exception $e) {
+            parent::getExceptionAsJSON($e);
+        }
     }
 
     public static function getLogradouroByBairroAndCidade() : void
