ec6558a5d2069526d03635610e05f9612b42e4b8
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-03-16 15:29:17 -0300

    adicionando métodos na controller

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -2,6 +2,8 @@
 
 namespace ApiCep\Controller;
 
+use Exception;
+
 abstract class Controller
 {
     public static function getResponseAsJSON($data)
@@ -15,15 +17,28 @@ abstract class Controller
         exit(json_encode($data));
     }
 
-    public static function getExceptionAsJSON($data)
+    public static function getExceptionAsJSON(Exception $e)
     {
+
+        $exception = [
+            'message' => $e->getMessage(),
+            'code' => $e->getCode(),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'traceAsString' => $e->getTraceAsString(),
+            'previous' => $e->getPrevious()
+        ];
+
         header("Access-Control-Allow-Origin: *");
         header("Content-Type: application/json; charset=utf-8");
         header("Cache-Control: no-cache, must-revalidate");
         header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
         header("Pragma: public");
 
-        exit(json_encode($data));
+        http_response_code(400);
+
+        exit(json_encode($exception));
+        
     }
 }
 
